ba323e9d9f55487cf7ffc8d57e654a734739fa30
blackxfiied <41133734+blackxfiied@users.noreply.github.com>
2023-09-29 22:55:00 +0800

    Basic Sparkle implementation and appcast creation on appcastify

--- Mythic/Utilities/Base/UpdaterSettings.swift
+++ Mythic/Utilities/Base/UpdaterSettings.swift
@@ -5,14 +5,39 @@
 //  Created by Esiayo Alegbe on 29/9/2023.
 //
 
+// Reference: https://sparkle-project.org/documentation/preferences-ui/#adding-settings-in-swiftui
+
 import SwiftUI
+import Sparkle
 
-struct UpdaterSettings: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+// This is the view for our updater settings
+// It manages local state for checking for updates and automatically downloading updates
+// Upon user changes to these, the updater's properties are set. These are backed by NSUserDefaults.
+// Note the updater properties should *only* be set when the user changes the state.
+struct UpdaterSettingsView: View {
+    private let updater: SPUUpdater
+    
+    @State private var automaticallyChecksForUpdates: Bool
+    @State private var automaticallyDownloadsUpdates: Bool
+    
+    init(updater: SPUUpdater) {
+        self.updater = updater
+        self.automaticallyChecksForUpdates = updater.automaticallyChecksForUpdates
+        self.automaticallyDownloadsUpdates = updater.automaticallyDownloadsUpdates
     }
-}
-
-#Preview {
-    UpdaterSettings()
+    
+    var body: some View {
+        VStack {
+            Toggle("Automatically check for updates", isOn: $automaticallyChecksForUpdates)
+                .onChange(of: automaticallyChecksForUpdates) { newValue in
+                    updater.automaticallyChecksForUpdates = newValue
+                }
+            
+            Toggle("Automatically download updates", isOn: $automaticallyDownloadsUpdates)
+                .disabled(!automaticallyChecksForUpdates)
+                .onChange(of: automaticallyDownloadsUpdates) { newValue in
+                    updater.automaticallyDownloadsUpdates = newValue
+                }
+        }.padding()
+    }
 }
