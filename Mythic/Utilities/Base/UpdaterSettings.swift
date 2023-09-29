d136813f1c22c4fe153930f32bf9f96f41a926b5
blackxfiied <41133734+blackxfiied@users.noreply.github.com>
2023-09-29 22:54:23 +0800

    File hierarchy adjustment

---
+++ Mythic/Utilities/Base/UpdaterSettings.swift
@@ -0,0 +1,18 @@
+//
+//  UpdaterSettings.swift
+//  Mythic
+//
+//  Created by Esiayo Alegbe on 29/9/2023.
+//
+
+import SwiftUI
+
+struct UpdaterSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdaterSettings()
+}
