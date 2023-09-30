727aaff81fa58b31055b9925369a850ec7b8118f
blackxfiied <41133734+blackxfiied@users.noreply.github.com>
2023-09-30 12:41:34 +0800

    Add uninstalling support to Mythic

--- Mythic/Views/Base/GameList/Extensions/GameUninstall.swift
+++ Mythic/Views/Base/GameList/Extensions/GameUninstall.swift
@@ -12,23 +12,70 @@ extension GameListView {
         @Binding var isPresented: Bool
         @Binding var game: String
         
+        @State private var keepFiles: Bool = false
+        @State private var skipUninstaller: Bool = false
+        
+        @State private var isConfirmationPresented = false
+        
         var body: some View {
-            NotImplemented()
-            
             VStack {
-                Text(game)
+                Text("Uninstall \(game)")
                     .font(.title)
                 
                 Spacer()
                 
                 HStack {
-                    Button(action: {
-                        isPresented.toggle()
-                    }) {
-                        Text("Close")
+                    Toggle(isOn: $keepFiles) {
+                        Text("Keep files")
                     }
+                    Spacer()
+                }
+                
+                HStack {
+                    Toggle(isOn: $skipUninstaller) {
+                        Text("Don't run uninstaller")
+                    }
+                    Spacer()
+                }
+                
+                HStack {
+                    Button("Cancel", role: .cancel) {
+                        isPresented.toggle()
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Uninstall") {
+                        isConfirmationPresented = true
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                .alert(isPresented: $isConfirmationPresented) {
+                    Alert(
+                        title: Text("Are you sure you want to delete \(game)?"),
+                        primaryButton: .destructive(Text("Delete")) {
+                            _ = Legendary.command(
+                                args: [
+                                    "-y",
+                                    "uninstall",
+                                    keepFiles ? "--keep-files" : nil,
+                                    skipUninstaller ? "--skip-uninstaller" : nil,
+                                    game
+                                ]
+                                    .compactMap { $0 },
+                                useCache: false
+                            )
+                            
+                            isConfirmationPresented = false
+                            isPresented = false
+                        },
+                        secondaryButton: .cancel(Text("Cancel")) {
+                            isConfirmationPresented = false
+                        }
+                    )
                 }
             }
+            .padding()
         }
     }
 }
