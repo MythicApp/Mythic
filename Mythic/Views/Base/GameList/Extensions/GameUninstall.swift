fcf470043c24a7a6ebd26a731c7dcae8c38927a0
blackxfiied <41133734+blackxfiied@users.noreply.github.com>
2023-10-05 00:54:11 +0800

    Finally fixed the issue in the game uninstaller where the error alert doesn't show.

--- Mythic/Views/Base/GameList/Extensions/GameUninstall.swift
+++ Mythic/Views/Base/GameList/Extensions/GameUninstall.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import OSLog
 
 extension GameListView {
     struct UninstallView: View {
@@ -13,12 +14,17 @@ extension GameListView {
         @Binding var game: String
         @Binding var isGameListRefreshCalled: Bool
         
+        enum ActiveAlert {
+            case error, confirmation
+        }
+        
         @State private var keepFiles: Bool = false
         @State private var skipUninstaller: Bool = false
         
-        @State private var isErrorPresented = false
-        @State private var isConfirmationPresented = false
         @State private var isProgressViewSheetPresented = false
+        
+        @State private var isAlertPresented = false
+        @State private var activeAlert: ActiveAlert = .confirmation
         
         @State private var errorContent: Substring = ""
         
@@ -51,7 +57,8 @@ extension GameListView {
                     Spacer()
                     
                     Button("Uninstall") {
-                        isConfirmationPresented = true
+                        activeAlert = .confirmation
+                        isAlertPresented = true
                     }
                     .buttonStyle(.borderedProminent)
                 }
@@ -62,65 +69,63 @@ extension GameListView {
                 ProgressViewSheet(isPresented: $isProgressViewSheetPresented)
             }
             
-            .alert(isPresented: $isErrorPresented) { // not working...
-                Alert(
-                    title: Text("Error uninstalling game"),
-                    message: Text(errorContent)
-                )
-            }
-            
-            .alert(isPresented: $isConfirmationPresented) {
-                Alert(
-                    title: Text("Are you sure you want to delete \(game)?"),
-                    primaryButton: .destructive(Text("Delete")) {
-                        isProgressViewSheetPresented = true
-                        
-                        DispatchQueue.global(qos: .userInteractive).async { [self] in
-                            let commandOutput = Legendary.command(
-                                args: [
-                                    "-y",
-                                    "uninstall",
-                                    keepFiles ? "--keep-files" : nil,
-                                    skipUninstaller ? "--skip-uninstaller" : nil,
-                                    game
-                                ]
-                                    .compactMap { $0 },
-                                useCache: false
-                            )
+            .alert(isPresented: $isAlertPresented) {
+                switch activeAlert {
+                case .error:
+                    Alert(
+                        title: Text("Error uninstalling game"),
+                        message: Text(errorContent)
+                    )
+                case .confirmation:
+                    Alert(
+                        title: Text("Are you sure you want to delete \(game)?"),
+                        primaryButton: .destructive(Text("Delete")) {
+                            isProgressViewSheetPresented = true
                             
-                            if let commandStderrString = String(data: commandOutput.stderr, encoding: .utf8) {
-                                if !commandStderrString.isEmpty {
-                                    if commandStderrString.contains("INFO: Game has been uninstalled.") {
-                                        isProgressViewSheetPresented = false
-                                        isPresented = false
-                                        isGameListRefreshCalled = true
-                                    }
-                                }
+                            DispatchQueue.global(qos: .userInteractive).async { [self] in
+                                let commandOutput = Legendary.command(
+                                    args: [
+                                        "-y",
+                                        "uninstall",
+                                        keepFiles ? "--keep-files" : nil,
+                                        skipUninstaller ? "--skip-uninstaller" : nil,
+                                        game
+                                    ]
+                                        .compactMap { $0 },
+                                    useCache: false
+                                )
                                 
-                                for line in commandStderrString.components(separatedBy: "\n") {
-                                    print("line \(line)")
-                                    if line.contains("ERROR:") {
-                                        print("line \(line)")
-                                        if let range = line.range(of: "ERROR: ") {
-                                            print("range \(line)")
-                                            let substring = line[range.upperBound...]
-                                            print("substring \(substring)")
-                                            errorContent = substring
+                                if let commandStderrString = String(data: commandOutput.stderr, encoding: .utf8) {
+                                    if !commandStderrString.isEmpty {
+                                        if commandStderrString.contains("INFO: Game has been uninstalled.") {
                                             isProgressViewSheetPresented = false
-                                            isErrorPresented = true // only one alert works for some reason (definitely a bug)
-                                            break // first error only
+                                            isPresented = false
+                                            isGameListRefreshCalled = true
+                                        }
+                                    }
+                                    
+                                    for line in commandStderrString.components(separatedBy: "\n") {
+                                        if line.contains("ERROR:") {
+                                            if let range = line.range(of: "ERROR: ") {
+                                                let substring = line[range.upperBound...]
+                                                Logger.app.error("Error found when attempting to delete game.")
+                                                errorContent = substring
+                                                isProgressViewSheetPresented = false
+                                                activeAlert = .error
+                                                isAlertPresented = true
+                                                break // first error only
+                                            }
                                         }
                                     }
                                 }
                             }
+                        },
+                        secondaryButton: .cancel(Text("Cancel")) {
+                            isAlertPresented = false
                         }
-                    },
-                    secondaryButton: .cancel(Text("Cancel")) {
-                        isConfirmationPresented = false
-                    }
-                )
+                    )
+                }
             }
-
         }
     }
 }
