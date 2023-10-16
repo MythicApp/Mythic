fdeea09b153a56d82950b137afa1752e51726677
blackxfiied <41133734+blackxfiied@users.noreply.github.com>
2023-10-16 22:36:10 +0800

    Forgot to remove test items in Support.swift

--- Mythic/Views/Navigation/Support.swift
+++ Mythic/Views/Navigation/Support.swift
@@ -10,10 +10,6 @@ import SwiftUI
 struct SupportView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .onAppear {
-                print(isAppInstalled(bundleIdentifier: "com.isaacmarovitz.Whisky"))
-                print(try! Legendary.isAlias(game: "amongus"))
-            }
     }
 }
 
