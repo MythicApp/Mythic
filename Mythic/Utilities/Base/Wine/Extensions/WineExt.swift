38a4181177b079305467f6f841264dda35176d25
blackxfiied <41133734+blackxfiied@users.noreply.github.com>
2023-11-07 18:05:19 +0800

    Wine backend interface creation.

---
+++ Mythic/Utilities/Base/Wine/Extensions/WineExt.swift
@@ -0,0 +1,14 @@
+//
+//  Bottles.swift
+//  Mythic
+//
+//  Created by Esiayo Alegbe on 30/10/2023.
+//
+
+import Foundation
+
+fileprivate let files = FileManager.default
+
+extension Wine {
+    
+}
---
+++ Mythic/Utilities/Base/Wine/WineInterface.swift
@@ -0,0 +1,38 @@
+//
+//  Wine.swift
+//  Mythic
+//
+//  Created by Esiayo Alegbe on 30/10/2023.
+//
+
+import Foundation
+import OSLog
+
+fileprivate let files = FileManager.default
+
+class Wine {
+    
+    @available(*, message: "Not implemented.")
+    static func command() {
+        
+    }
+    
+    static let bottlesDirectory = {
+        let directory = Bundle.appContainer!.appending(path: "Bottles")
+        if !files.fileExists(atPath: directory.path) {
+            do {
+                try files.createDirectory(at: directory, withIntermediateDirectories: false)
+                Logger.file.info("Creating bottles directory")
+            }
+            catch { Logger.app.error("Error creating Bottles directory: \(error)") }
+        }
+        
+        return directory
+    }
+    
+    func createBottle(name: String) {
+        guard Libraries.isInstalled() else { return }
+        
+        // run wineboot
+    }
+}
