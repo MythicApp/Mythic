a0bd6bf7328cd39f7f4b90b6cc084259c562cafb
Jeremy Bosma <jectaunscripted@gmail.com>
2023-11-17 19:44:18 +0100

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- Mythic/Views/Navigation/Support.swift
+++ Mythic/Views/Navigation/Support.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SupportView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Under construction")
     }
 }
 
