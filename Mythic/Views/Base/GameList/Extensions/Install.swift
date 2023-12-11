d9eb0c62bdcc11b2d61118c36a2d0d312e42dc42
blackxfiied <41133734+blackxfiied@users.noreply.github.com>
2023-12-11 23:31:27 +0800

    Added licensing information within every script as per the license's suggestion.

--- Mythic/Views/Base/GameList/Extensions/Install.swift
+++ Mythic/Views/Base/GameList/Extensions/Install.swift
@@ -5,6 +5,12 @@
 //  Created by Esiayo Alegbe on 29/9/2023.
 //
 
+// Copyright © 2023 blackxfiied
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+
 import SwiftUI
 
 extension GameListView {
