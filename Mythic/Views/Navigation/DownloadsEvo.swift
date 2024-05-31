385adcf43515b1250a64fde4a391804eefc83b32
blackxfiied <41133734+blackxfiied@users.noreply.github.com>
2024-05-31 22:33:17 +0800

    Refactor downloads view

--- Mythic/Views/Navigation/DownloadsEvo.swift
+++ Mythic/Views/Navigation/DownloadsEvo.swift
@@ -5,6 +5,13 @@
 //  Created by Esiayo Alegbe on 5/4/2024.
 //
 
+// MARK: - Copyright
+// Copyright © 2023 blackxfiied, Jecta
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+
 import SwiftUI
 import CachedAsyncImage
 import Shimmer
@@ -14,12 +21,10 @@ struct DownloadsEvo: View {
     @State private var cardsUpdated: Bool = false
     
     var body: some View {
-        if operation.current?.game != nil {
+        if let currentGame = operation.current?.game {
             VStack {
-                DownloadCard(game: operation.current!.game, style: .prominent)
-                
+                DownloadCard(game: currentGame, style: .prominent)
                 Divider()
-                
                 if operation.queue.isEmpty {
                     Text("No other downloads are pending.")
                         .bold()
@@ -30,160 +35,119 @@ struct DownloadsEvo: View {
                     }
                     .animation(.easeInOut, value: cardsUpdated)
                 }
-                
                 Spacer()
             }
             .padding()
+        } else {
+            Text("No other downloads are pending.")
+                .font(.bold(.title)())
         }
     }
 }
 
 struct DownloadCard: View {
-    
     @ObservedObject private var operation: GameOperation = .shared
     @State private var isHoveringOverDestructiveButton: Bool = false
     
     var game: Game
     var style: DownloadCardStyle
+    
     enum DownloadCardStyle {
         case normal, prominent
+    }
+    
+    private var statusText: Text {
+        if operation.current?.game == game {
+            return .init("\(operation.current?.type.rawValue.uppercased() ?? "MODIFYING") \(Image(systemName: "arrow.down.circle"))")
+        } else if operation.queue.contains(where: { $0.game == game }) {
+            return .init("QUEUED \(Image(systemName: "stopwatch"))")
+        }
+        return .init("MODIFYING")
+    }
+    
+    private var progressText: Text {
+        let progress = Int(operation.status.progress?.percentage ?? 0)
+        let speed = Int(operation.status.downloadSpeed?.raw ?? 0.0 * (1000000 / 1048576))
+        let eta = operation.status.progress?.eta ?? "00:00:00"
+        return .init("\(progress)% • ↓ \(speed) MB/s • ⏲︎ \(eta)")
     }
     
     var body: some View {
         RoundedRectangle(cornerRadius: 20)
             .fill(.background)
             .frame(maxHeight: 120)
-            .opacity({
-                switch style {
-                case .normal:
-                    return 1
-                case .prominent:
-                    return 0
+            .opacity(style == .prominent ? 0 : 1)
+            .overlay {
+                CachedAsyncImage(url: URL(string: Legendary.getImage(of: game, type: .normal))) { phase in
+                    switch phase {
+                    case .empty:
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.windowBackground)
+                            .shimmering(animation: .easeInOut(duration: 1).repeatForever(autoreverses: false), bandSize: 1)
+                    case .success(let image):
+                        if case .prominent = style {
+                            image.resizable()
+                                .blur(radius: 20.0)
+                                .modifier(FadeInModifier())
+                        } else {
+                            image.resizable()
+                                .blur(radius: 20.0)
+                                .clipShape(.rect(cornerRadius: 20))
+                                .modifier(FadeInModifier())
+                        }
+                    case .failure:
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.windowBackground)
+                            .overlay { Image(systemName: "exclamationmark.triangle.fill") }
+                    @unknown default:
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.windowBackground)
+                            .overlay { Image(systemName: "questionmark.circle.fill") }
+                    }
                 }
-            }())
-            .overlay(alignment: .leading) {
+                .ignoresSafeArea()
+                
                 HStack {
-                    CachedAsyncImage(url: .init(string: Legendary.getImage(of: game, type: .normal))) { phase in
-                        switch phase {
-                        case .empty:
-                            switch style {
-                            case .normal:
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(.windowBackground)
-                                    .shimmering(
-                                        animation: .easeInOut(duration: 1)
-                                            .repeatForever(autoreverses: false),
-                                        bandSize: 1
-                                    )
-                            case .prominent:
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(.windowBackground)
-                                    .shimmering(
-                                        animation: .easeInOut(duration: 1)
-                                            .repeatForever(autoreverses: false),
-                                        bandSize: 1
-                                    )
-                            }
-                        case .success(let image):
-                            switch style {
-                            case .normal:
-                                image
-                                    .resizable()
-                                    .blur(radius: 20.0)
-                                    .clipShape(.rect(cornerRadius: 20))
-                                    .modifier(FadeInModifier())
-                            case .prominent:
-                                image
-                                    .resizable()
-                                    .blur(radius: 20.0)
-                                    .modifier(FadeInModifier())
-                            }
-                        case .failure:
-                            // fallthrough
-                            RoundedRectangle(cornerRadius: 20)
-                                .fill(.windowBackground)
-                                .overlay {
-                                    Image(systemName: "exclamationmark.triangle.fill")
-                                }
-                        @unknown default:
-                            RoundedRectangle(cornerRadius: 20)
-                                .fill(.windowBackground)
-                                .overlay {
-                                    Image(systemName: "questionmark.circle.fill")
-                                }
-                        }
-                    }
-                    .overlay(alignment: .leading) {
+                    VStack(alignment: .leading) {
+                        statusText
+                            .foregroundStyle(.secondary)
+                            .font(style == .normal ? .caption : .callout)
+                        
                         HStack {
-                            VStack {
-                                HStack {
-                                    Text(
-                                        operation.current?.game == game ? "DOWNLOADING \(Image(systemName: "arrow.down.circle"))" :
-                                            operation.queue.contains(where: { $0.game == game }) ? "QUEUED \(Image(systemName: "stopwatch"))" : "Unknown \(Image(systemName: "questionmark.circle"))"
-                                    )
-                                    .foregroundStyle(.secondary)
-                                    .font({
-                                        switch style {
-                                        case .normal:
-                                            return .caption
-                                        case .prominent:
-                                            return .callout
-                                        }
-                                    }())
-                                    
-                                    Spacer()
-                                }
-                                
-                                HStack {
-                                    Text(game.title)
-                                        .font(.bold({
-                                            switch style {
-                                            case .normal:
-                                                return .title
-                                            case .prominent:
-                                                return .largeTitle
-                                            }
-                                        }())())
-                                    SubscriptedTextView(game.type.rawValue)
-                                    
-                                    Spacer()
-                                }
-                                
-                                if operation.current?.game == game {
-                                    HStack {
-                                        Text("\(Int(operation.status.progress?.percentage ?? 0))% • ↓ \(Int(operation.status.downloadSpeed?.raw ?? 0.0) * (1000000/1048576)) MB/s • ⏲︎ \(operation.status.progress?.eta ?? "00:00:00")")
-                                            .foregroundStyle(.tertiary)
-                                            .font({
-                                                switch style {
-                                                case .normal:
-                                                    return .caption
-                                                case .prominent:
-                                                      return .callout
-                                                }
-                                            }())
-                                        
-                                        Spacer()
-                                    }
-                                }
-                            }
-                            .foregroundStyle(.white)
-                            
-                            if operation.current?.game == game {
-                                GameInstallProgressView(withPercentage: false)
-                            } else if operation.queue.contains(where: { $0.game == game }) {
-                                Button {
-                                    operation.queue.removeAll(where: { $0.game == game })
-                                } label: {
-                                    Image(systemName: "minus")
-                                        .padding(5)
-                                        .foregroundStyle(isHoveringOverDestructiveButton ? .red : .primary)
-                                }
-                                .clipShape(.circle)
-                                .help("Remove from download queue")
+                            Text(game.title)
+                                .font(.system(style == .normal ? .title : .largeTitle, weight: .bold))
+                            SubscriptedTextView(game.type.rawValue)
+                            Spacer()
+                        }
+                        
+                        if operation.current?.game == game {
+                            HStack {
+                                progressText
+                                    .foregroundStyle(.tertiary)
+                                    .font(style == .normal ? .caption : .callout)
+                                Spacer()
                             }
                         }
-                        .padding(.horizontal)
                     }
+                    .foregroundStyle(.white)
+                    .padding(.horizontal)
+                    
+                    Group {
+                        if operation.current?.game == game {
+                            GameInstallProgressView(withPercentage: false)
+                        } else if operation.queue.contains(where: { $0.game == game }) {
+                            Button {
+                                operation.queue.removeAll(where: { $0.game == game })
+                            } label: {
+                                Image(systemName: "minus")
+                                    .padding(5)
+                                    .foregroundStyle(isHoveringOverDestructiveButton ? .red : .primary)
+                            }
+                            .clipShape(.circle)
+                            .help("Remove from download queue")
+                        }
+                    }
+                    .padding(.trailing)
                 }
             }
     }
@@ -191,8 +155,4 @@ struct DownloadCard: View {
 
 #Preview {
     DownloadsEvo()
-    /*
-     DownloadCard(game: .constant(.init(type: .epic, title: "")))
-     .frame(height: 150)
-     */
 }
