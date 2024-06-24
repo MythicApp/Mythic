b1028c7e6cdf2f6e17d725ac352b2765cd3d6ecd
Marcus Ziadé <guitaripod@icloud.com>
2024-06-24 07:37:51 -0700

    Add missing files and update from main

---
+++ Mythic/Utilities/SparkleController.swift
@@ -0,0 +1,18 @@
+import Foundation
+import Sparkle
+
+final class SparkleController: ObservableObject {
+    var updaterController: SPUStandardUpdaterController
+
+    var updater: SPUUpdater {
+        updaterController.updater
+    }
+
+    init() {
+        updaterController = SPUStandardUpdaterController(
+            startingUpdater: true,
+            updaterDelegate: nil,
+            userDriverDelegate: nil
+        )
+    }
+}
