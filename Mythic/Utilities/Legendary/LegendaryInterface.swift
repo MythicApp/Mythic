c9fad7b5909f7d3266cedf3bcd901f0d0ec17bab
blackxfiied <41133734+blackxfiied@users.noreply.github.com>
2024-10-06 14:55:17 +0800

    fix logic error again?

--- Mythic/Utilities/Legendary/LegendaryInterface.swift
+++ Mythic/Utilities/Legendary/LegendaryInterface.swift
@@ -360,7 +360,7 @@ final class Legendary {
             throw GameDoesNotExistError(game)
         }
         
-        if !Engine.exists, case .windows = game.platform { throw Engine.NotInstalledError() }
+        if game.platform == .windows && Engine.exists == false { throw Engine.NotInstalledError() }
         
         guard let bottleURL = game.bottleURL else { throw Wine.BottleDoesNotExistError() } // FIXME: Bottle Revamp
         let bottle = try Wine.getBottleObject(url: bottleURL)
