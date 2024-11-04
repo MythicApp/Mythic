4295296b029aaa74ad8e5e1424ef7eecf46390c2
esiayo <41133734+blackxfiied@users.noreply.github.com>
2024-11-04 18:44:58 +0800

    feat: add uninstrusive one-time rpc bridge installation prompt

--- Mythic/MythicApp.swift
+++ Mythic/MythicApp.swift
@@ -25,8 +25,22 @@ struct MythicApp: App {
     
     // MARK: - State Properties
     @AppStorage("isOnboardingPresented") var isOnboardingPresented: Bool = true
+    @AppStorage("hasPresentedRPCBridgePrompt") var hasPresentedRPCBridgePrompt: Bool = false
+
     @State var onboardingPhase: OnboardingR2.Phase = .allCases.first!
-    
+
+    @State private var isDiscordRPCBridgeAlertPresented: Bool = false
+    private var isDiscordInstalled: Bool {
+        ["", "PTB", "Canary"].contains { isAppInstalled(bundleIdentifier: "com.hnc.Discord\($0)") }
+    }
+    var shouldIgnoreDiscordRPCPromptCheck: Bool {
+        #if !DEBUG
+        return !hasPresentedRPCBridgePrompt
+        #else
+        return true
+        #endif
+    }
+
     @StateObject private var networkMonitor: NetworkMonitor = .init()
     @StateObject private var sparkleController: SparkleController = .init()
     
@@ -41,7 +55,62 @@ struct MythicApp: App {
             window.standardWindowButton(.zoomButton)?.isHidden = !value
         }
     }
-    
+
+    func installRPCBridge() {
+        Task(priority: .userInitiated) {
+            do {
+                try await Engine.RPCBridge.modifyLaunchAgent(.install)
+                let alert = NSAlert()
+                alert.messageText = "Discord RPC Bridge installed successfully."
+                alert.informativeText = """
+                Supported Windows® games will now show their activity in Discord.
+                You can disable this functionality at any time in Mythic Settings.
+                """
+                if let window = NSApp.windows.first {
+                    await alert.beginSheetModal(for: window)
+                }
+            } catch {
+                let errorAlert = NSAlert()
+                errorAlert.alertStyle = .critical
+                errorAlert.messageText = "Unable to install Discord RPC Bridge."
+                errorAlert.informativeText = """
+                \(error.localizedDescription)
+                Please try again in Mythic Settings.
+                """
+                errorAlert.addButton(withTitle: "OK")
+
+                if let window = NSApp.windows.first {
+                    await errorAlert.beginSheetModal(for: window)
+                }
+            }
+        }
+    }
+
+    func presentRPCBridgeInstallationCancellationAlert() {
+        let alert = NSAlert()
+        alert.messageText = "Are you sure you want to cancel?"
+        alert.informativeText = "You can enable Discord RPC Bridge later by going to Mythic's settings."
+
+        let installButton = alert.addButton(withTitle: "Install")
+        installButton.hasDestructiveAction = true
+
+        alert.addButton(withTitle: "Cancel")
+
+        if let window = NSApp.windows.first {
+            alert.beginSheetModal(for: window) { response in
+                if case .alertFirstButtonReturn = response {
+                    installRPCBridge()
+                }
+
+                if case .OK = response {
+                    #if !DEBUG
+                    hasPresentedRPCBridgePrompt = true
+                    #endif
+                }
+            }
+        }
+    }
+
     // MARK: - App Body
     var body: some Scene {
         Window("Mythic", id: "main") {
@@ -62,7 +131,27 @@ struct MythicApp: App {
                     .environmentObject(networkMonitor)
                     .environmentObject(sparkleController)
                     .frame(minWidth: 750, minHeight: 390)
-                    .onAppear { toggleTitleBar(true) }
+                    .onAppear {
+                        toggleTitleBar(true)
+
+                        if isDiscordInstalled,
+                           !Engine.RPCBridge.launchAgentInstalled,
+                           shouldIgnoreDiscordRPCPromptCheck {
+                            isDiscordRPCBridgeAlertPresented = true
+                        }
+                    }
+                    .alert(isPresented: $isDiscordRPCBridgeAlertPresented) {
+                        .init(
+                            title: .init("Install Discord RPC Bridge? (Beta)"),
+                            message: .init("""
+                            Mythic has detected that you have Discord installed.
+                            Discord RPC Bridge is a beta feature that allows Windows® games to display their presence in Discord.
+                            Would you like to install it?
+                            """),
+                            primaryButton: .default(.init("Install"), action: installRPCBridge),
+                            secondaryButton: .cancel(.init("Cancel"), action: presentRPCBridgeInstallationCancellationAlert)
+                        )
+                    }
             }
         }
 
