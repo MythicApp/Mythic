a2f741da2de8a4158c433193f07c9ee5c8bb34cc
esiayo <41133734+blackxfiied@users.noreply.github.com>
2024-11-09 09:46:07 +0800

    feat: add 0.4.2 to `WhatsNewCollection` + move `WhatsNewCollection` to a different file + build bump

--- Mythic/MythicApp.swift
+++ Mythic/MythicApp.swift
@@ -77,7 +77,8 @@ struct MythicApp: App {
                         UserDefaultsWhatsNewVersionStore()
 #endif
                     }(),
-                whatsNewCollection: self)
+                whatsNewCollection: self
+             )
         )
 
         .commands {
@@ -102,47 +103,6 @@ struct MythicApp: App {
         }
          */
     }
-}
-
-extension MythicApp: WhatsNewCollectionProvider {
-    var whatsNewCollection: WhatsNewCollection {
-        WhatsNew(
-            version: "0.4.1",
-            title: "What's new in Mythic",
-            features: [
-                .init(
-                    image: .init(
-                        systemName: "ladybug",
-                        foregroundColor: .red
-                    ),
-                    title: "Bug Fixes & Performance Improvements",
-                    subtitle: "Y'know, the usual."
-                ),
-                .init(
-                    image: .init(
-                        systemName: "checklist",
-                        foregroundColor: .blue
-                    ),
-                    title: "Optional Pack support",
-                    subtitle: "Epic Games that support selective downloads are now supported for download (e.g. Fortnite)."
-                ),
-                .init(
-                    image: .init(
-                        systemName: "cursorarrow.motionlines",
-                        foregroundColor: .accent
-                    ),
-                    title: "More animations",
-                    subtitle: "Added smooth animations and transitions."
-                )
-            ],
-            primaryAction: .init(),
-            secondaryAction: .init(
-                title: "Learn more",
-                action: .openURL(.init(string: "https://github.com/MythicApp/Mythic/releases/tag/0.4.1"))
-            )
-        )
-    }
-
 }
 
 #Preview {
---
+++ Mythic/WhatsNewCollection.swift
@@ -0,0 +1,89 @@
+//
+//  WhatsNewCollection.swift
+//  Mythic
+//
+//  Created by Esiayo Alegbe on 11/9/24.
+//
+
+import WhatsNewKit
+import SwiftUI
+
+extension MythicApp: WhatsNewCollectionProvider {
+    var whatsNewCollection: WhatsNewCollection {
+        WhatsNew(
+            version: "0.4.1",
+            title: "What's new in Mythic",
+            features: [
+                .init(
+                    image: .init(
+                        systemName: "ladybug",
+                        foregroundColor: .red
+                    ),
+                    title: "Bug Fixes & Performance Improvements",
+                    subtitle: "Y'know, the usual."
+                ),
+                .init(
+                    image: .init(
+                        systemName: "checklist",
+                        foregroundColor: .blue
+                    ),
+                    title: "Optional Pack support",
+                    subtitle: "Epic Games that support selective downloads are now supported for download (e.g. Fortnite)."
+                ),
+                .init(
+                    image: .init(
+                        systemName: "cursorarrow.motionlines",
+                        foregroundColor: .accent
+                    ),
+                    title: "More animations",
+                    subtitle: "Added smooth animations and transitions."
+                )
+            ],
+            primaryAction: .init(),
+            secondaryAction: .init(
+                title: "Learn more",
+                action: .openURL(.init(string: "https://github.com/MythicApp/Mythic/releases/tag/0.4.1"))
+            )
+        )
+
+        WhatsNew(
+            version: "0.4.2",
+            title: "What's new in Mythic",
+            features: [
+                .init(
+                    image: .init(
+                        systemName: "ladybug.slash",
+                        foregroundColor: .red
+                    ),
+                    title: "Bugfixes",
+                    subtitle: "Y'know, the usual."
+                ),
+                .init(
+                    image: .init(
+                        systemName: "app.badge.checkmark",
+                        foregroundColor: .cyan
+                    ),
+                    title: "Fixed crashes when verifying imported games",
+                    subtitle: "Someone even called it a 'glorified exit button..'"
+                ),
+                .init(
+                    image: .init(
+                        systemName: "square.badge.plus.fill",
+                        foregroundColor: .pink
+                    ),
+                    title: "Fixed launch arguments not saving",
+                    subtitle: "Protip: you can use [-nomovie] to skip the intro in Rocket League®."
+                )
+            ],
+            primaryAction: .init(),
+            secondaryAction: .init(
+                title: "Learn more",
+                action: .openURL(.init(string: "https://github.com/MythicApp/Mythic/releases/tag/0.4.2"))
+            )
+        )
+    }
+}
+
+#Preview {
+    WhatsNewView(whatsNew: MythicApp().whatsNewCollection.last ?? WhatsNew(title: "N/A", features: []))
+}
