d8ede3d158f2c1bec376876492ae7967471f6a0b
hans <127169913+dxrkinfuser44@users.noreply.github.com>
2024-11-27 22:42:11 +1100

    Update Bottles.swift
    also made this more performant
    
    Signed-off-by: hans <127169913+dxrkinfuser44@users.noreply.github.com>

--- Mythic/Views/Navigation/Bottles.swift
+++ Mythic/Views/Navigation/Bottles.swift
@@ -7,8 +7,8 @@
 
 // Copyright © 2023 blackxfiied, Jecta
 
-// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the Licen[...]
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Gener[...]
 // You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
 
 // You can fold these comments by pressing [⌃ ⇧ ⌘ ◀︎], unfold with [⌃ ⇧ ⌘ ▶︎]
@@ -22,46 +22,57 @@ struct BottlesView: View {
     var body: some View {
         BottleListView()
             .navigationTitle("Bottles")
-        
-            .task(priority: .background) {
-                discordRPC.setPresence({
-                    var presence: RichPresence = .init()
-                    presence.details = "Managing their Windows® Instances"
-                    presence.state = "Managing bottles"
-                    presence.timestamps.start = .now
-                    presence.assets.largeImage = "macos_512x512_2x"
-                    
-                    return presence
-                }())
+            .backgroundTask {
+                updateDiscordPresence()
             }
-        
             .toolbar {
                 if Engine.exists {
-                    ToolbarItem(placement: .confirmationAction) {
-                        Button {
-                            isBottleCreationViewPresented = true
-                        } label: {
-                            Image(systemName: "plus")
-                        }
-                        .help("Add a bottle")
-                    }
-                    
-                    if let bottlesDirectory = Wine.bottlesDirectory {
-                        ToolbarItem(placement: .confirmationAction) {
-                            Button {
-                                workspace.open(bottlesDirectory)
-                            } label: {
-                                Image(systemName: "folder")
-                            }
-                            .help("Open Bottles directory")
-                        }
+                    ToolbarItemGroup {
+                        AddBottleButton(isPresented: $isBottleCreationViewPresented)
+                        OpenBottlesDirectoryButton()
                     }
                 }
             }
-        
             .sheet(isPresented: $isBottleCreationViewPresented) {
                 BottleCreationView(isPresented: $isBottleCreationViewPresented)
             }
+    }
+    
+    private func updateDiscordPresence() {
+        discordRPC.setPresence({
+            var presence: RichPresence = .init()
+            presence.details = "Managing their Windows® Instances"
+            presence.state = "Managing bottles"
+            presence.timestamps.start = .now
+            presence.assets.largeImage = "macos_512x512_2x"
+            return presence
+        }())
+    }
+}
+
+struct AddBottleButton: View {
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        Button {
+            isPresented = true
+        } label: {
+            Image(systemName: "plus")
+        }
+        .help("Add a bottle")
+    }
+}
+
+struct OpenBottlesDirectoryButton: View {
+    var body: some View {
+        if let bottlesDirectory = Wine.bottlesDirectory {
+            Button {
+                workspace.open(bottlesDirectory)
+            } label: {
+                Image(systemName: "folder")
+            }
+            .help("Open Bottles directory")
+        }
     }
 }
 
