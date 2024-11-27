1bc40f3474a6737b25c3952b1251b563544e8448
hans <127169913+dxrkinfuser44@users.noreply.github.com>
2024-11-27 22:44:17 +1100

    Update DownloadsViewEvo.swift
    removed the unnecessary updates and made the for loop efficient
    
    Signed-off-by: hans <127169913+dxrkinfuser44@users.noreply.github.com>

--- Mythic/Views/Navigation/DownloadsViewEvo.swift
+++ Mythic/Views/Navigation/DownloadsViewEvo.swift
@@ -5,13 +5,6 @@
 //  Created by Esiayo Alegbe on 5/4/2024.
 //
 
-// MARK: - Copyright
-// Copyright © 2023 blackxfiied, Jecta
-
-// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-// You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
-
 import SwiftUI
 import CachedAsyncImage
 import Shimmer
@@ -32,10 +25,12 @@ struct DownloadsEvo: View {
                         .bold()
                         .padding()
                 } else {
-                    ForEach(operation.queue, id: \.self) { args in
-                        DownloadCard(game: args.game, style: .normal)
+                    LazyVStack {
+                        ForEach(operation.queue, id: \.self) { args in
+                            DownloadCard(game: args.game, style: .normal)
+                        }
+                        .animation(.easeInOut, value: cardsUpdated)
                     }
-                    .animation(.easeInOut, value: cardsUpdated)
                 }
                 
                 Spacer()
