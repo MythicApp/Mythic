1193284238de7a7c2205d150da344dc2040393fc
hans <127169913+dxrkinfuser44@users.noreply.github.com>
2024-11-27 22:49:25 +1100

    Update Store.swift
    smaller and more efficient
    
    Signed-off-by: hans <127169913+dxrkinfuser44@users.noreply.github.com>

--- Mythic/Views/Navigation/Store.swift
+++ Mythic/Views/Navigation/Store.swift
@@ -5,102 +5,86 @@
 //  Created by Esiayo Alegbe on 10/9/2023.
 //
 
-// Copyright © 2023 blackxfiied, Jecta
-
-// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-// You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
-
-// You can fold these comments by pressing [⌃ ⇧ ⌘ ◀︎], unfold with [⌃ ⇧ ⌘ ▶︎]
-
 import SwiftUI
 import SwordRPC
 
 struct StoreView: View {
     @State private var loadingError: Error?
-    @State private var isLoading: Bool? = false
+    @State private var isLoading: Bool = false
     @State private var canGoBack = false
     @State private var canGoForward = false
     @State private var url: URL = .init(string: "https://store.epicgames.com/")!
-    @State private var refreshAnimation: Angle = .degrees(0)
+    @State private var refreshAnimation: Double = 0
     
     var body: some View {
-        WebView(url: url, error: .constant(nil), isLoading: .constant(nil), canGoBack: canGoBack, canGoForward: canGoForward)
-        
-        .navigationTitle("Store")
-        
-        .task(priority: .background) {
-            discordRPC.setPresence({
-                var presence: RichPresence = .init()
-                presence.details = "Currently browsing \(url)"
-                presence.state = "Looking for games to purchase"
-                presence.timestamps.start = .now
-                presence.assets.largeImage = "macos_512x512_2x"
+        WebView(url: url, error: $loadingError, isLoading: $isLoading, canGoBack: $canGoBack, canGoForward: $canGoForward)
+            .navigationTitle("Store")
+            .task(priority: .background) {
+                updateDiscordPresence()
+            }
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button(action: goBack) {
+                        Image(systemName: "arrow.left.circle")
+                    }
+                    .disabled(!canGoBack)
+                }
                 
-                return presence
-            }())
-        }
-        
-        .toolbar {
-            // FIXME: Loading view update creates view update race condition with webview
-            /*
-             if isLoading {
                 ToolbarItem(placement: .confirmationAction) {
-                    ProgressView()
-                        .progressViewStyle(.circular)
-                        .controlSize(.small)
+                    Button(action: goForward) {
+                        Image(systemName: "arrow.right.circle")
+                    }
+                    .disabled(!canGoForward)
                 }
-             } else if loadingError {
+                
                 ToolbarItem(placement: .confirmationAction) {
-                    Image(systemName: "exclamationmark.triangle.fill")
-                        .symbolEffect(.pulse)
-                }
-             }
-             */
-            
-            ToolbarItem(placement: .confirmationAction) {
-                Button {
-                    if canGoBack {
-                        url = .init(string: "javascript:history.back();")!
+                    Button(action: refreshPage) {
+                        Image(systemName: "arrow.clockwise.circle")
+                            .rotationEffect(.degrees(refreshAnimation))
                     }
-                } label: {
-                    Image(systemName: "arrow.left.circle")
                 }
-                .disabled(!canGoBack)
-            }
-            
-            ToolbarItem(placement: .confirmationAction) {
-                Button {
-                    if canGoForward {
-                        url = .init(string: "javascript:history.forward();")!
+                
+                ToolbarItem(placement: .confirmationAction) {
+                    Button(action: openInBrowser) {
+                        Image(systemName: "arrow.up.forward")
                     }
-                } label: {
-                    Image(systemName: "arrow.right.circle")
-                }
-                .disabled(!canGoForward)
-            }
-            
-            ToolbarItem(placement: .confirmationAction) {
-                Button {
-                    url = .init(string: "javascript:location.reload();")!
-                    withAnimation(.default) {
-                        refreshAnimation = .degrees(360)
-                    } completion: {
-                        refreshAnimation = .degrees(0)
-                    }
-                } label: {
-                    Image(systemName: "arrow.clockwise.circle")
-                        .rotationEffect(refreshAnimation) // thx whisky
                 }
             }
-            ToolbarItem(placement: .confirmationAction) {
-                Button {
-                    workspace.open(url)
-                } label: {
-                    Image(systemName: "arrow.up.forward")
-                }
-            }
+    }
+    
+    private func updateDiscordPresence() {
+        discordRPC.setPresence {
+            var presence = RichPresence()
+            presence.details = "Currently browsing \(url)"
+            presence.state = "Looking for games to purchase"
+            presence.timestamps.start = .now
+            presence.assets.largeImage = "macos_512x512_2x"
+            return presence
+        }()
+    }
+    
+    private func goBack() {
+        if canGoBack {
+            url = URL(string: "javascript:history.back();")!
         }
+    }
+    
+    private func goForward() {
+        if canGoForward {
+            url = URL(string: "javascript:history.forward();")!
+        }
+    }
+    
+    private func refreshPage() {
+        url = URL(string: "javascript:location.reload();")!
+        withAnimation {
+            refreshAnimation = 360
+        }
+        refreshAnimation = 0
+    }
+    
+    private func openInBrowser() {
+        workspace.open(url)
     }
 }
 
