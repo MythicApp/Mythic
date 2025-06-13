b2aa262691375ce9e49ada545e0585d347bfef19
esiayo <41133734+vapidinfinity@users.noreply.github.com>
2025-06-13 10:08:03 +0800

    feat: add sentence casing to errors thrown by steam

---
+++ Mythic/Utilities/Extensions/Built-in/String.swift
@@ -0,0 +1,16 @@
+//
+//  String.swift
+//  Mythic
+//
+//  Created by Esiayo Alegbe on 13/6/2025.
+//
+
+extension String {
+    var sentenceCased: String {
+        guard !self.isEmpty else { return self }
+        let lowercased = self.lowercased()
+        let first = lowercased.prefix(1).capitalized
+        let rest = lowercased.dropFirst()
+        return first + rest + "."
+    }
+}
