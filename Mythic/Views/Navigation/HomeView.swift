MythicApp/Mythic

commit 18da44b9609a427a860c5c7fdebd731ffb8cb53a

feat: add `ContentUnavailableView` in cases where the app does not have a wide image + use vstack in favour of overlay + build bump

Mythic/Views/Navigation/HomeView.swift
@@ -35,8 +35,8 @@ struct HomeView: View {
         GeometryReader { geometry in
             ScrollView {
                 if var recentGame = try? defaults.decodeAndGet(Game.self, forKey: "recentlyPlayed") {
-                    VStack {
-                        if recentGame.wideImageURL != nil {
+                    ZStack(alignment: .bottomLeading) {
+                        if recentGame.wideImageURL == nil {
                             AsyncImage(url: recentGame.wideImageURL) { phase in
                                 switch phase {
                                 case .empty:
@@ -79,10 +79,20 @@ struct HomeView: View {
                                     .background(.quinary)
                                 }
                             }
+                        } else {
+                            ContentUnavailableView(
+                                "Image Unavailable",
+                                systemImage: "photo.badge.exclamationmark",
+                                description: .init("""
+                                This game doesn't have a widescreen image that Mythic can display.
+                                """)
+                            )
+                            .frame(
+                                width: geometry.size.width,
+                                height: geometry.size.height * 0.75
+                            )
                         }
-                    }
-                    .frame(height: geometry.size.height * 0.75)
-                    .overlay(alignment: .bottomLeading) {
+
                         VStack(alignment: .leading) {
                             Text("CONTINUE PLAYING")
                                 .foregroundStyle(.placeholder)
@@ -111,6 +121,7 @@ struct HomeView: View {
                         .padding([.leading, .bottom])
                         .foregroundStyle(.white)
                     }
+                    .frame(height: geometry.size.height * 0.75)
                 } else {
                     // TODO: move to relevant location
                     ContentUnavailableView(
@@ -120,8 +131,10 @@ struct HomeView: View {
                         This area is where your most recently played game will appear — try launching one now!
                         """)
                     )
-                    .frame(width: geometry.size.width,
-                           height: geometry.size.height * 0.75)
+                    .frame(
+                        width: geometry.size.width,
+                        height: geometry.size.height * 0.75
+                    )
                     .background(.quinary)
                 }
 

Mythic.xcodeproj/project.pbxproj
@@ -835,7 +835,7 @@
 				"CODE_SIGN_IDENTITY[sdk=macosx*]" = "-";
 				CODE_SIGN_STYLE = Automatic;
 				COMBINE_HIDPI_IMAGES = YES;
-				CURRENT_PROJECT_VERSION = 3554;
+				CURRENT_PROJECT_VERSION = 3555;
 				DEAD_CODE_STRIPPING = YES;
 				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
 				DEVELOPMENT_ASSET_PATHS = "\"Mythic/Preview Content\"";
@@ -882,7 +882,7 @@
 				"CODE_SIGN_IDENTITY[sdk=macosx*]" = "-";
 				CODE_SIGN_STYLE = Automatic;
 				COMBINE_HIDPI_IMAGES = YES;
-				CURRENT_PROJECT_VERSION = 3554;
+				CURRENT_PROJECT_VERSION = 3555;
 				DEAD_CODE_STRIPPING = YES;
 				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
 				DEVELOPMENT_ASSET_PATHS = "\"Mythic/Preview Content\"";

commit 638d3b3cf72f1b0807f41ef2aa6d5d554d065089

revert: logic error introduced in 18da44b

Mythic/Views/Navigation/HomeView.swift
@@ -43,7 +43,7 @@ struct HomeView: View {
             ScrollView {
                 if var recentGame = try? defaults.decodeAndGet(Game.self, forKey: "recentlyPlayed") {
                     ZStack(alignment: .bottomLeading) {
-                        if recentGame.wideImageURL == nil {
+                        if recentGame.wideImageURL != nil {
                             AsyncImage(url: recentGame.wideImageURL) { phase in
                                 switch phase {
                                 case .empty: