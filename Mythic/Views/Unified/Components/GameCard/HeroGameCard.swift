5367c708b60cf43939e6d7bb4c224f6d3ac9d861
esiayo <41133734+vapidinfinity@users.noreply.github.com>
2025-11-09 09:02:52 +0800

    feat: add background extension effect to hero game card views (the community has spoken almost unanimously 😭) + build bump

--- Mythic/Views/Unified/Components/GameCard/HeroGameCard.swift
+++ Mythic/Views/Unified/Components/GameCard/HeroGameCard.swift
@@ -25,78 +25,87 @@ extension HeroGameCard {
         @Binding var isImageEmpty: Bool
 
         var body: some View {
-            if game.wideImageURL != nil {
-                AsyncImage(url: game.wideImageURL) { phase in
-                    switch phase {
-                    case .empty:
-                        Rectangle()
-                            .fill(.quinary)
-                            .shimmering(
-                                animation: .easeInOut(duration: 1)
-                                    .repeatForever(autoreverses: false),
-                                bandSize: 1
+            Group {
+                if game.wideImageURL != nil {
+                    AsyncImage(url: game.wideImageURL) { phase in
+                        switch phase {
+                        case .empty:
+                            Rectangle()
+                                .fill(.quinary)
+                                .shimmering(
+                                    animation: .easeInOut(duration: 1)
+                                        .repeatForever(autoreverses: false),
+                                    bandSize: 1
+                                )
+                                .onAppear {
+                                    withAnimation { isImageEmpty = true }
+                                }
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .glur(radius: 20, offset: 0.65, interpolation: 0.7)
+                                .modifier(FadeInModifier())
+                                .onAppear {
+                                    withAnimation { isImageEmpty = false }
+                                }
+                        case .failure(let error):
+                            ContentUnavailableView(
+                                "Unable to load the image.",
+                                systemImage: "photo.badge.exclamationmark",
+                                description: .init(error.localizedDescription)
                             )
+                            .background(.quinary)
                             .onAppear {
                                 withAnimation { isImageEmpty = true }
                             }
-                    case .success(let image):
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .glur(radius: 20, offset: 0.65, interpolation: 0.7)
-                            .modifier(FadeInModifier())
-                            .onAppear {
-                                withAnimation { isImageEmpty = false }
-                            }
-                    case .failure(let error):
-                        ContentUnavailableView(
-                            "Unable to load the image.",
-                            systemImage: "photo.badge.exclamationmark",
-                            description: .init(error.localizedDescription)
-                        )
-                        .background(.quinary)
-                        .onAppear {
-                            withAnimation { isImageEmpty = true }
-                        }
-                    @unknown default:
-                        ContentUnavailableView(
-                            "Unable to load the image.",
-                            systemImage: "photo.badge.exclamationmark",
-                            description: .init("""
+                        @unknown default:
+                            ContentUnavailableView(
+                                "Unable to load the image.",
+                                systemImage: "photo.badge.exclamationmark",
+                                description: .init("""
                         Please check your connection, and try again.
                         """)
-                        )
-                        .background(.quinary)
-                        .onAppear {
-                            withAnimation { isImageEmpty = true }
+                            )
+                            .background(.quinary)
+                            .onAppear {
+                                withAnimation { isImageEmpty = true }
+                            }
                         }
                     }
-                }
-            } else {
-                ContentUnavailableView(
-                    "Image Unavailable",
-                    systemImage: "photo.badge.exclamationmark",
-                    description: .init("""
+                } else {
+                    ContentUnavailableView(
+                        "Image Unavailable",
+                        systemImage: "photo.badge.exclamationmark",
+                        description: .init("""
                 This game doesn't have a widescreen image that Mythic can display.
                 """)
-                )
+                    )
+                }
+            }
+            .customTransform { view in
+                if #available(macOS 26.0, *) {
+                    view.backgroundExtensionEffect()
+                } else {
+                    view
+                }
             }
         }
     }
+}
 
-    #Preview {
-        GeometryReader { geometry in
-            VStack {
-                HeroGameCard.ImageCard(
-                    game: .constant(.init(source: .local, title: "test", platform: .macOS, path: "")),
-                    isImageEmpty: .constant(false)
-                )
-                .frame(width: geometry.size.width, height: geometry.size.height * 0.75)
+#Preview {
+    GeometryReader { geometry in
+        VStack {
+            HeroGameCard.ImageCard(
+                game: .constant(.init(source: .local, title: "test", platform: .macOS, path: "")),
+                isImageEmpty: .constant(false)
+            )
+            .frame(width: geometry.size.width, height: geometry.size.height * 0.75)
 
-                Divider()
+            Divider()
 
-                Text("Content below the card")
-            }
+            Text("Content below the card")
         }
     }
 }
