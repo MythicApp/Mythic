3d052c4e7670fb496df558b6f4423213d5e577d1
esiayo <41133734+vapidinfinity@users.noreply.github.com>
2025-11-11 22:14:10 +0800

    feat: ensure proper concurrency safety of `VariableManager`, and add deprecation message

--- Mythic/Utilities/VariableManager.swift
+++ Mythic/Utilities/VariableManager.swift
@@ -26,27 +26,35 @@ import Foundation
  variableManager.setVariable("hello", value: "hi")
  ```
  */
-@MainActor
-@Observable class VariableManager: ObservableObject, @unchecked Sendable {
+@available(*, deprecated, message: "Prefer the use of structured classes or environment objects. This class will be removed shortly.")
+@MainActor @Observable class VariableManager: ObservableObject, @unchecked Sendable {
     static let shared: VariableManager = .init()
+    let lock: NSLock = .init()
+
     private init() { }
     
     private var variables = [String: Any]()
     
     /// Set variable data within the variable manager.
     func setVariable(_ key: String, value: Any) {
-        self.objectWillChange.send()
-        self.variables[key] = value
+        lock.withLock {
+            self.objectWillChange.send()
+            self.variables[key] = value
+        }
     }
     
     /// Retrieve variable data from the variable manager.
     func getVariable<T>(_ key: String) -> T? {
-        return variables[key] as? T
+        lock.withLock {
+            return variables[key] as? T
+        }
     }
     
     /// Remove variable data from the variable manager.
     func removeVariable(_ key: String) {
-        self.objectWillChange.send()
-        self.variables[key] = nil
+        lock.withLock {
+            self.objectWillChange.send()
+            self.variables[key] = nil
+        }
     }
 }
