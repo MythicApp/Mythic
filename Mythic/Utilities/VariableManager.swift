1b8129a17057f0c57598559645c96f76db66fef6
esiayo <41133734+vapidinfinity@users.noreply.github.com>
2025-11-23 15:46:25 +0800

    fix: remove `NSLock` from `VariableManager` since it's made redundant by `@MainActor`

--- Mythic/Utilities/VariableManager.swift
+++ Mythic/Utilities/VariableManager.swift
@@ -11,15 +11,15 @@ import Foundation
 
 /**
  A flexible class that enables cross-script variables.
- 
+
  Example usage:
- 
+
  SwiftUI
  ```
  @ObservedObject private var variables: VariableManager = .shared
  variableManager.setVariable("hello", value: "hi")
  ```
- 
+
  Swift
  ```
  let variableManager: VariableManager = .shared
@@ -27,34 +27,27 @@ import Foundation
  ```
  */
 @available(*, deprecated, message: "Prefer the use of structured classes or environment objects. This class will be removed shortly.")
-@MainActor @Observable class VariableManager: ObservableObject, @unchecked Sendable {
+@MainActor class VariableManager: ObservableObject, @unchecked Sendable {
     static let shared: VariableManager = .init()
-    let lock: NSLock = .init()
 
     private init() { }
-    
+
     private var variables = [String: Any]()
-    
+
     /// Set variable data within the variable manager.
     func setVariable(_ key: String, value: Any) {
-        lock.withLock {
-            self.objectWillChange.send()
-            self.variables[key] = value
-        }
+        self.objectWillChange.send()
+        self.variables[key] = value
     }
-    
+
     /// Retrieve variable data from the variable manager.
     func getVariable<T>(_ key: String) -> T? {
-        lock.withLock {
-            return variables[key] as? T
-        }
+        return variables[key] as? T
     }
-    
+
     /// Remove variable data from the variable manager.
     func removeVariable(_ key: String) {
-        lock.withLock {
-            self.objectWillChange.send()
-            self.variables[key] = nil
-        }
+        self.objectWillChange.send()
+        self.variables[key] = nil
     }
 }
