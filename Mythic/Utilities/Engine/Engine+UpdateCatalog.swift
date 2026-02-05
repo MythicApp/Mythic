7dd8c430af8ce62d5d5718b69e0297c6524be9d3
esi <41133734+vapidinfinity@users.noreply.github.com>
2026-02-05 13:30:59 +0800

    feat!(`UpdateCatalog`): refactor + version 0.2.0

--- Mythic/Utilities/Engine/Engine+UpdateCatalog.swift
+++ Mythic/Utilities/Engine/Engine+UpdateCatalog.swift
@@ -1,5 +1,5 @@
 //
-//  Engine+Appcast.swift
+//  Engine+UpdateCatalog.swift
 //  Mythic
 //
 //  Created by vapidinfinity (esi) on 24/10/2025.
@@ -10,58 +10,60 @@
 import Foundation
 import SemanticVersion
 
-// swiftlint:disable nesting
 extension Engine {
-    struct UpdateCatalog: Codable {
+    struct UpdateCatalog: Decodable {
         /// Native `UpdateCatalog` version this struct is based upon.
-        static let nativeVersion: SemanticVersion = .init(0, 1, 0)
+        static let nativeVersion: SemanticVersion = .init(0, 2, 0)
 
         let version: SemanticVersion
         let lastUpdated: Date
         let channels: [Channel]
-
-        struct Channel: Codable {
-            let name: ReleaseChannel
-            let releases: [Release]
-
-            var latestCompatibleRelease: Release? {
-                releases
-                    .filter { // exclude releases w/ unfulfilled app version requirement
-                        guard let minimumAppVersion = $0.minimumAppVersion,
-                              let appVersion = Mythic.appVersion
-                        else { return true } // keep if appVersion unverifiable/no minimumAppVersion
-                        return minimumAppVersion <= appVersion
-                    }
-                    .filter { // exclude releases w/ unfulfilled macOS version requirement
-                        guard let minimumMacOSVersion = $0.minimumMacOSVersion,
-                              let macOSVersion = SemanticVersion.macOSVersion
-                        else { return true }
-                        return minimumMacOSVersion <= macOSVersion
-                    }
-                    .max(by: { $0.version < $1.version }) // get version w/ highest (thus newest) version
-            }
-        }
-
-        struct Release: Codable {
-            let version: SemanticVersion
-            let releaseDate: Date
-            let downloadURL: String // cannot use URL type, URLs are stored as dicts for some strange reason
-            let checksumURL: String?
-            let size: Int?
-            let minimumMacOSVersion: SemanticVersion? // upstream MUST have a patch version or this will fail to decode
-            let minimumAppVersion: SemanticVersion?
-            let releaseNotesURL: String?
-            let critical: Bool
-            let commitSHA: String
-        }
     }
-
+    
     enum ReleaseChannel: String, Codable {
         case stable
         case preview
     }
 }
-// swiftlint:enable nesting
+
+extension Engine.UpdateCatalog {
+    struct Channel: Decodable {
+        let name: Engine.ReleaseChannel
+        let releases: [Release]
+
+        var latestCompatibleRelease: Release? {
+            releases
+                .filter { // exclude releases w/ unfulfilled app version requirement
+                    guard let minimumAppVersion = $0.minimumAppVersion,
+                          let appVersion = Mythic.appVersion
+                    else { return true } // keep if appVersion unverifiable/no minimumAppVersion
+                    return minimumAppVersion <= appVersion
+                    // FIXME: on dirty builds, the prerelease identifier makes the version less than the minimum app version.
+                }
+                .filter { // exclude releases w/ unfulfilled macOS version requirement
+                    guard let minimumMacOSVersion = $0.minimumMacOSVersion,
+                          let macOSVersion = SemanticVersion.macOSVersion
+                    else { return true }
+                    return minimumMacOSVersion <= macOSVersion
+                }
+                .max(by: { $0.version < $1.version }) // get version w/ highest (thus newest) version
+        }
+    }
+    
+    struct Release: Decodable {
+        let version: SemanticVersion
+        let releaseDate: Date
+        let downloadURL: String // cannot use URL type, URLs are stored as dicts for some strange reason
+        let checksumURL: String?
+        let size: Int?
+        let minimumMacOSVersion: SemanticVersion? // upstream MUST have a patch version or this will fail to decode
+        let minimumAppVersion: SemanticVersion?
+        var targetGPTKVersion: SemanticVersion
+        let releaseNotesURL: String?
+        let critical: Bool
+        let commitSHA: String
+    }
+}
 
 extension Collection where Element == Engine.UpdateCatalog.Channel {
     // typed lookup by enum
